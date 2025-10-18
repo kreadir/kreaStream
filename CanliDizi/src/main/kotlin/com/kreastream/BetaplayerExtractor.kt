ee5fcf1ef4baf20097ae01749b690d89a5f80406
Kreadir <kreadir@outlook.com>
2025-10-18 15:15:46 +0300

    Add files via upload

--- CanliDizi/src/main/kotlin/com/kreastream/BetaplayerExtractor.kt
+++ CanliDizi/src/main/kotlin/com/kreastream/BetaplayerExtractor.kt
@@ -1,15 +1,10 @@
 package com.kreastream
 
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.ExtractorApi
 import com.lagradost.cloudstream3.utils.*
 
-class BetaplayerExtractor : ExtractorApi() {
-    override val name = "Betaplayer"
-    override val mainUrl = "https://betaplayer.site"
-    override val requiresReferer = false
-
-    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
+class BetaplayerExtractor {
+    suspend fun extract(url: String): List<ExtractorLink> {
         val doc = app.get(url).document
         val videoUrl = doc.selectFirst("video source")?.attr("src")
             ?: Regex("""file:\s*["'](https?://[^"']+)["']""").find(doc.html())?.groupValues?.getOrNull(1)
--- CanliDizi/src/main/kotlin/com/kreastream/CanliplayerExtractor.kt
+++ CanliDizi/src/main/kotlin/com/kreastream/CanliplayerExtractor.kt
@@ -1,15 +1,10 @@
 package com.kreastream
 
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.ExtractorApi
 import com.lagradost.cloudstream3.utils.*
 
-class CanliplayerExtractor : ExtractorApi() {
-    override val name = "Canliplayer"
-    override val mainUrl = "https://canliplayer.com"
-    override val requiresReferer = false
-
-    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
+class CanliplayerExtractor {
+    suspend fun extract(url: String): List<ExtractorLink> {
         val doc = app.get(url).document
         val videoUrl = doc.selectFirst("video source")?.attr("src")
             ?: Regex("""file:\s*["'](https?://[^"']+)["']""").find(doc.html())?.groupValues?.getOrNull(1)
