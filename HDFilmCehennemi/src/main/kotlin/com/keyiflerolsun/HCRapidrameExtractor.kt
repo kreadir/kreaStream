035a3fd15de7076a801f3cdbe98e7948e681efc8
Kreadir <kreadir@outlook.com>
2025-10-20 17:19:53 +0300

    Update HCRapidrameExtractor.kt

--- HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HCRapidrameExtractor.kt
+++ HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HCRapidrameExtractor.kt
@@ -3,7 +3,7 @@ package com.keyiflerolsun
 class HCRapidrameExtractor : HCCloseLoadExtractor() {
 
     override var name    = "Rapidrame"
-    override var mainUrl = "https://www.hdfilmcehennemi.nl"
+    override var mainUrl = "https://www.hdfilmcehennemi.la"
 
 
 }
