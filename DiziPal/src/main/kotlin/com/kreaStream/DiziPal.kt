3d155d7056f8311a3baa3c8291998fa7558a7c54
Kreadir <kreadir@outlook.com>
2025-10-21 23:27:48 +0300

    Rename rating variable to score in DiziPal.kt

--- DiziPal/src/main/kotlin/com/kreaStream/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/kreaStream/DiziPal.kt
@@ -167,7 +167,7 @@ class DiziPal : MainAPI() {
         val year        = document.selectXpath("//div[text()='Yapım Yılı']//following-sibling::div").text().trim().toIntOrNull()
         val description = document.selectFirst("div.summary p")?.text()?.trim()
         val tags        = document.selectXpath("//div[text()='Türler']//following-sibling::div").text().trim().split(" ").map { it.trim() }
-        val rating      = document.selectXpath("//div[text()='IMDB Puanı']//following-sibling::div").text().trim().toRatingInt()
+        val score      = document.selectXpath("//div[text()='IMDB Puanı']//following-sibling::div").text().trim().toRatingInt()
         val duration    = Regex("(\\d+)").find(document.selectXpath("//div[text()='Ortalama Süre']//following-sibling::div").text())?.value?.toIntOrNull()
 
         if (url.contains("/dizi/")) {
@@ -191,7 +191,7 @@ class DiziPal : MainAPI() {
                 this.year      = year
                 this.plot      = description
                 this.tags      = tags
-                this.score     = Score.from10(rating)
+                this.score     = Score.from10(score)
                 this.duration  = duration
             }
         } else { 
@@ -202,7 +202,7 @@ class DiziPal : MainAPI() {
                 this.year      = year
                 this.plot      = description
                 this.tags      = tags
-                this.score     = Score.from10(rating)
+                this.score     = Score.from10(score)
                 this.duration  = duration
             }
         }
