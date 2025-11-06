fd6909c45ca03be7146fe4bee30fed7be38c4ba3
Kreadir <kreadir@outlook.com>
2025-11-07 00:36:26 +0300

    Update mainUrl to new DiziPal domain

--- DiziPalOrijinal/src/main/kotlin/com/kreastream/DiziPalOrijinal.kt
+++ DiziPalOrijinal/src/main/kotlin/com/kreastream/DiziPalOrijinal.kt
@@ -28,7 +28,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 
 
 class DiziPalOrijinal : MainAPI() {
-    override var mainUrl = "https://dizipal1507.com"
+    override var mainUrl = "https://dizipal1510.com"
     override var name = "DiziPal Orijinal"
     override val hasMainPage = true
     override var lang = "tr"
