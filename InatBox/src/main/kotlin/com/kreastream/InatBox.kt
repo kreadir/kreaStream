ce8d7e55749b10943acf6083bbad6f7ccb55a6e9
Kreadir <kreadir@outlook.com>
2025-11-28 16:44:24 +0300

    Comment out specific content URLs
    Comment out URLs for TOD and Spor Kanalları.

--- InatBox/src/main/kotlin/com/kreastream/InatBox.kt
+++ InatBox/src/main/kotlin/com/kreastream/InatBox.kt
@@ -40,10 +40,10 @@ class InatBox : MainAPI() {
         "${contentUrl}/amz/index.php"                               to "Amazon Prime",
         "${contentUrl}/hb/index.php"                                to "HBO Max",
         "${contentUrl}/tbi/index.php"                               to "Tabii",
-        "https://boxggboxss.cfd/CDN/001_STR/boxggboxss.cfd/ccc/index.php"     to "TOD",
+        //"https://boxggboxss.cfd/CDN/001_STR/boxggboxss.cfd/ccc/index.php"     to "TOD",
         "${contentUrl}/tv/sinema.php"                               to "Sinema Kanalları",
         "${contentUrl}/tv/ulusal.php"                               to "Ulusal Kanallar",
-        "https://boxggboxss.cfd/CDN/001_STR/boxggboxss.cfd/spor_v2.php"       to "Spor Kanalları",
+        //"https://boxggboxss.cfd/CDN/001_STR/boxggboxss.cfd/spor_v2.php"       to "Spor Kanalları",
         
         //"${contentUrl}/film/mubi.php"                             to "Mubi",
         //"${contentUrl}/tv/list1.php"                              to "Kanallar Liste 1 ",
